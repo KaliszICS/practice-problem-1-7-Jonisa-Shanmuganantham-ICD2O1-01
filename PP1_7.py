becb3021b8631598f6254f081e967b6d7b4527ea
Jonisa-Shanmuganantham-ICD2O1-01 <jonisa.shanmuganantham@student.tdsb.on.ca>
2024-09-25 14:55:05 +0000

    abc

--- PP1_7.py
+++ PP1_7.py
@@ -1,18 +1,26 @@
+'''
+    Lesson: Booleans
+    Author: Jonisa Shanmuganantham
+    Date Creatd: Sept 25, 2024
+    Date Last Modified: Sept 25, 2024
+'''
 
+defq1():
+  bool1 = 5 < 10
+  print(bool1)
 
-q1():
+defq2():
+  integer = input(Input an integer: )
+  bool1  = integer > 5
+  print = bool1
+  
+defq3():
   #Write code here
 
-q2():
+defq4():
   #Write code here
 
-q3():
-  #Write code here
-
-q4():
-  #Write code here
-
-q5():
+defq5():
   #Write code here
 
 #Do edit the code below
