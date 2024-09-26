d14187ed3d94de805aa76a6c364e039ad141393d
Jonisa-Shanmuganantham-ICD2O1-01 <jonisa.shanmuganantham@student.tdsb.on.ca>
2024-09-26 13:20:55 +0000

    first commit

--- PP1_7.py
+++ PP1_7.py
@@ -1,33 +1,42 @@
 '''
     Lesson: Booleans
     Author: Jonisa Shanmuganantham
-    Date Creatd: Sept 25, 2024
-    Date Last Modified: Sept 25, 2024
+    Date Created: Sept 25, 2024
+    Date Last Modified: Sept 26, 2024
 '''
 
-defq1():
+def q1():
   bool1 = 5 < 10
   print(bool1)
 
-defq2():
-  integer = input(Input an integer: )
+def q2():
+  integer = input("Input an integer: ")
+  integer = int(integer)
   bool1  = integer > 5
   print = bool1
   
-defq3():
-  #Write code here
+def q3():
+  word = input("Input the letter a: ")
+  bool1 = word == "a"
+  print(bool1)
 
-defq4():
-  #Write code here
+def q4():
+  word = input("Input a word earlier in the dictionary than google: ")
+  bool1 = word < "google"
 
-defq5():
-  #Write code here
+def q5():
+  integer = input("Input an integer: ")
+  integer2 = input("Input another integer: ")
+  integer = int(integer)
+  integer2 = int(integer2)
+  num = integer * integer2
+  bool1 = num > 40
+  print(f"Your numbers multiplied together are greater than 40: {bool1}")
 
-#Do edit the code below
-#Comment the lines below when running your tests
-
+'''
 q1()
 q2()
 q3()
 q4()
 q5()
+'''
